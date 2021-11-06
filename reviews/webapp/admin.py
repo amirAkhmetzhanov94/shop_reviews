dd4c2435bcfbed28a7ca51c9e32ed9fb9b65d4df
Amir Akhmetzhanov <amir.akhmetzhanov94@gmail.com>
2021-11-06 12:04:22 +0600

    Создал модели 'Product' и 'Review'. Прописал вывод моделей в админ-панель, добавил фикстуры, создал миграции на основе написанных моделей.

--- reviews/webapp/admin.py
+++ reviews/webapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from webapp.models import Review, Product
+
+admin.site.register(Review)
+admin.site.register(Product)
+
+
--- reviews/webapp/models.py
+++ reviews/webapp/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Product(models.Model):
+    CATEGORY_CHOICES = [('Smartphones', 'Smartphones'), ('Laptops', 'Laptops'), ('Appliances', 'Appliances')]
+    name = models.CharField(max_length=30, blank=False, null=False, verbose_name="Name")
+    category = models.CharField(max_length=20, blank=False, null=False, choices=CATEGORY_CHOICES,
+                                verbose_name="Category")
+    description = models.CharField(max_length=2000, verbose_name="Description")
+    picture = models.ImageField(upload_to="pictures", verbose_name="Picture")
+
+    def __str__(self):
+        return f"{self.name} ({self.category})"
+
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+
+
+class Review(models.Model):
+    RATING_CHOICES = [('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5')]
+    author = models.ForeignKey(get_user_model(), on_delete=models.SET_DEFAULT, default=1, verbose_name="Author")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name="Product", related_name="reviews")
+    text = models.CharField(max_length=1500, null=False, blank=False)
+    rating = models.CharField(max_length=2, blank=False, null=False, choices=RATING_CHOICES, verbose_name="Rating")
+
+    def __str__(self):
+        return f"{self.product} ({self.author}, {self.rating})"
+
+    class Meta:
+        verbose_name = "Review"
+        verbose_name_plural = "Reviews"
